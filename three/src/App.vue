f5ed6b8ad3c3af3553e206c13392ecc692fbdb8c
lasertsole <56973455+lasertsole@users.noreply.github.com>
2023-04-16 19:32:53 +0800

    完成demo3结合gsap实现运动控制

--- three/src/App.vue
+++ three/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import Demo1 from './components/demo1.vue';
-import Demo2 from './components/demo2.vue';
+  import Demo1 from './components/demo1.vue';
+  import Demo2 from './components/demo2.vue';
+  import Demo3 from './components/demo3.vue';
 </script>
 
 <template>
   <!-- <Demo1></Demo1> -->
-  <Demo2></Demo2>
+  <!-- <Demo2></Demo2> -->
+  <Demo3></Demo3>
 </template>
 
 <style scoped>
